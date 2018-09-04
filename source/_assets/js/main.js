5a741247050e35416670a5825d332f3e490bbfeb
Victor Rodrigues <victor.rodrigues.dev@gmail.com>
2018-09-04 11:58:49 -0300

    first commit

---
+++ source/_assets/js/main.js
@@ -0,0 +1,93 @@
+window.SmoothScroll = require('smooth-scroll');
+window.$ = window.jQuery = require('jquery');
+
+window.ready = function ready(selector, time, time_per_item) {
+    time = time || 300;
+    time_per_item = time_per_item || 150;
+    document.querySelectorAll(selector).forEach(function(el) {
+        setTimeout(function() {
+            el.classList.add('ready');
+        }, time);
+        time = time + time_per_item;
+    });
+};
+
+window.scroll = function scroll(selector, time, time_per_item) {
+    time = time || 300;
+    time_per_item = time_per_item || 0;
+
+    document.querySelectorAll(selector).forEach(function(el) {
+        let listener = function() {
+            if (el.getBoundingClientRect().bottom - window.innerHeight < 0 ) {
+                setTimeout(function() {
+                    el.classList.add('ready');
+                    window.removeEventListener("scroll", listener);
+                }, time);
+                time = time + time_per_item
+            }
+        };
+        window.addEventListener('scroll', listener);
+        listener();
+    });
+
+};
+
+window.toggleMenu = function toggleMenu() {
+    let header = document.querySelector('.header-nav--pages');
+    header.classList.toggle('show');
+};
+
+window.addEventListener('load', function (event) {
+    console.info("[event] window loaded!");
+    ready('.section-main--text');
+    ready('.section-main--highlight--container', 1500, 600);
+    scroll('.img-phone');
+    scroll('#form-assunto', 500);
+    scroll('.section-contact--form--info', 100, 300);
+});
+
+let last_scroll = window.scrollY;
+
+let bindScrollHeader = function() {
+    if (window.scrollY > 0) {
+        document.querySelector('.the-header').classList.add('fixed');
+    } else {
+        document.querySelector('.the-header').classList.remove('fixed');
+    }
+
+    if (last_scroll >= window.scrollY) {
+        document.querySelector('.the-header').classList.remove('hide');
+    } else {
+        document.querySelector('.the-header').classList.add('hide');
+    }
+    last_scroll = window.scrollY;
+};
+
+window.addEventListener('load', function() {
+    document.querySelectorAll('.section-recomende--list--item, .section-recomende--anchor').forEach(function(el) {
+
+        el.addEventListener('click', function(event) {
+            document.querySelectorAll('.section-recomende--list--item, .section-recomende--anchor').forEach(function(el) { el.classList.remove('current') });
+
+            this.classList.add('current');
+
+            let content_id = this.getAttribute('data-for');
+            let content = document.getElementById(content_id);
+
+            document.querySelectorAll('.section-recomende--container--content').forEach(function(el) {
+                el.classList.add('hide');
+            });
+
+            content.classList.remove('hide');
+        });
+    });
+});
+
+if (document.querySelector('.the-header')) {
+    window.addEventListener('scroll', bindScrollHeader);
+    window.addEventListener('load', bindScrollHeader);
+}
+
+new SmoothScroll('a[href*="#"]');
+
+console.info("[load] main script loaded!");
---
+++ source/_layouts/_partials/footer.blade.php
@@ -0,0 +1,80 @@
+<footer class="footer container-fluid py-4">
+    <div class="row mx-0 px-3">
+        <div class="col-md-3 col-12 text-md-left text-center">
+            <div class="footer--logo mb-4">
+                <img src="/assets/images/logo.png" />
+            </div>
+            <p class="footer--paragraph">
+                Plataforma brasileira líder em marketing
+                digital para relacionamento one to one. A
+                All iN é uma unidade de marketing de relacionamento
+                digital da Locaweb.
+            </p>
+            <a href="https://jobs.i-hunter.com/locaweb" class="footer--paragraph--link" target="_blank">Trabalhe conosco.</a>
+
+            <div class="footer--social-media d-flex justify-content-between mt-5">
+                <a class="ico ico-facebook" target="_blank" href="https://www.facebook.com/AllinMarketingCloud"></a>
+                <a class="ico ico-instagram" target="_blank" href="https://www.instagram.com/allin_mktcloud/"></a>
+                <a class="ico ico-tweet" target="_blank" href="https://twitter.com/allin_mktcloud"></a>
+                <a class="ico ico-youtube" target="_blank" href="https://www.youtube.com/user/AllinOficial"></a>
+                <a class="ico ico-linkedin" target="_blank" href="https://www.linkedin.com/company/allinmarketingcloud"></a>
+                <a class="ico ico-google-plus" target="_blank" href="https://plus.google.com/u/0/b/115822252495243526120/+AllinBrMarketingCloud"></a>
+            </div>
+        </div>
+        <div class="col-md-6 col-12">
+            <ul class="footer--links-list list-unstyled d-flex flex-md-row flex-column text-center">
+                <li>
+                    <a href="https://allin.com.br/solucoes/">
+                        Soluções
+                    </a>
+                </li>
+                <li>
+                    <a href="https://allin.com.br/parceria/">
+                        Parceria
+                    </a>
+                </li>
+                <li>
+                    <a href="https://allin.com.br/news/">
+                        News
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-3 col-12 pt-5">
+            <!--<div class="row justify-content-md-end justify-content-center mx-0">-->
+            <!--<a id="rodape_link_busca" class="btn btn-simples btn-busca" href="#">-->
+            <!--<span>Busca </span>-->
+            <!--<i class="icone icone-busca"></i>-->
+            <!--</a>-->
+            <!--</div>-->
+
+            <div class="row justify-content-md-end justify-content-center mx-0">
+                <a id="rodape_link_topo" class="btn btn-simples btn-topo" href="#home">
+                    <span>Topo </span>
+                    <i class="icone icone-topo"></i>
+                </a>
+            </div>
+            <div class="row mt-4 justify-content-md-end justify-content-center mx-0 mb-5 mt-md-0 mt-5">
+                <div class="mt-5">
+                    <img src="/assets/images/selo.png" style="width: 200px" />
+                </div>
+            </div>
+        </div>
+        <form class="col-12 d-flex justify-content-md-end justify-content-center mb-4 sml_subscribe" method="post">
+            <input class="sml_hiddenfield" name="sml_subscribe" type="hidden" value="1">
+            <input class="footer--input-newsletter" name="sml_email" placeholder="Receba notícias por e-mail" />
+            <button class="sml_submitbtn footer--input-newsletter--ico" type="submit">
+                <i class="ico ico-letter"></i>
+            </button>
+
+        </form>
+        <div class="col-12 d-flex justify-content-md-between flex-md-row flex-column">
+            <div class="footer--logo-locaweb text-center text-md-left">
+                <img src="/assets/images/logo-locaweb.png" style="max-width: 170px" />
+            </div>
+            <p class="footer--policy">
+                @2018 All iN Marketing Cloud e/ou Locaweb. Política de Privacidade / Política Anti-Spam.
+            </p>
+        </div>
+    </div>
+</footer>
