3a1aea411e94bcc9ab37bd67a8e269cc63ce38f2
Victor Rodrigues <victor.rodrigues.dev@gmail.com>
2018-09-25 18:29:36 -0300

    parcial

---
+++ source/compreenda.blade.php
@@ -0,0 +1,9 @@
+@extends('_layouts.master', ['subheader' => [
+    ['#', 'Compreenda', $page->baseUrl."/assets/images/icons/ico-compreenda.png" ],
+    ['#', 'Personalize', $page->baseUrl."/assets/images/icons/ico-personalize.png" ],
+    ['#', 'Engaje', $page->baseUrl."/assets/images/icons/ico-engaje.png" ],
+]])
+
+@section('content')
+    <h2>New Page</h2>
+@endsection
---
+++ source/personalize.blade.php
@@ -0,0 +1,4 @@
+@extends('_layouts.master')
+@section('content')
+    <h2>New Page</h2>
+@endsection
