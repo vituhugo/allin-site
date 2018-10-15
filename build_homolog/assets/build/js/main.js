01c8b7035a6218fc2c630381a9ce9a7a843840eb
Victor <victor.rodrigues@lqdi.net>
2018-10-15 18:08:34 -0300

    Build for homolog

--- build_homolog/assets/build/js/main.js
+++ build_homolog/assets/build/js/main.js
@@ -13471,7 +13471,6 @@ window.Element&&!Element.prototype.closest&&(Element.prototype.closest=function(
 /***/ "./node_modules/webpack/buildin/global.js":
 /***/ (function(module, exports) {
 
-<<<<<<< HEAD
 var g;
 
 // This works in non-strict mode
@@ -13493,29 +13492,6 @@ try {
 // easier to handle this case. if(!global) { ...}
 
 module.exports = g;
-=======
-var g;
-
-// This works in non-strict mode
-g = (function() {
-	return this;
-})();
-
-try {
-	// This works if eval is allowed (see CSP)
-	g = g || Function("return this")() || (1,eval)("this");
-} catch(e) {
-	// This works if the window reference is available
-	if(typeof window === "object")
-		g = window;
-}
-
-// g can still be undefined, but nothing to do about it...
-// We return undefined, instead of nothing here, so it's
-// easier to handle this case. if(!global) { ...}
-
-module.exports = g;
->>>>>>> 1c5d329b9fff63547ce8523dc5a4915edaa39627
 
 
 /***/ }),
@@ -13625,11 +13601,7 @@ window.toggleMenu = function toggleMenu() {
     header.classList.toggle('show');
 };
 
-<<<<<<< HEAD
 window.addEventListener('ready', function () {
-=======
-window.addEventListener('load', function () {
->>>>>>> 1c5d329b9fff63547ce8523dc5a4915edaa39627
     document.querySelectorAll('.section-recomende--list--item, .section-recomende--anchor').forEach(function (el) {
 
         el.addEventListener('click', function (event) {
